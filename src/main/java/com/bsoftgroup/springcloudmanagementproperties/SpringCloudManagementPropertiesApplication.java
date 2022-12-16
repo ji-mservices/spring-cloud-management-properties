12a59f2a67cb5cb166576ea21d8ad44d484a15dc
JhonatanIT <jhonatan.ibatac@gmail.com>
2022-12-16 18:40:39 -0500

    build: initial commit

---
+++ src/main/java/com/bsoftgroup/springcloudmanagementproperties/SpringCloudManagementPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.bsoftgroup.springcloudmanagementproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudManagementPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudManagementPropertiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsoftgroup/springcloudmanagementproperties/SpringCloudManagementPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsoftgroup.springcloudmanagementproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudManagementPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
